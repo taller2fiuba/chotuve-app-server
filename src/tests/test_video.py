493bff706290065fe0c59afa6bdb056366735599
edjus <ejusto@fi.uba.ar>
2020-05-22 18:25:23 -0300

    Agregar usuario por defecto en el test de video

--- src/tests/test_video.py
+++ src/tests/test_video.py
@@ -2,13 +2,21 @@ import unittest
 import mock
 
 from tests.base import LoginMockTestCase
+from app import app
+
+CHOTUVE_MEDIA_URL = app.config.get('CHOTUVE_MEDIA_URL')
 
 class VideoTestCase(LoginMockTestCase):
     @mock.patch('media_server_api.requests.post')
     def test_post_agregar_video(self, mock_post):
         mock_post.return_value.json = lambda: {}
         mock_post.return_value.status_code = 200
-        response = self.app.post('/video', json={'url': 'value', 'titulo': 'data'})
+        body = {'url': 'value', 'titulo': 'data'}
+
+        response = self.app.post('/video', json=body)
+
+        body['usuario_id'] = 1 # agregar usuario por defecto
+        mock_post.assert_called_with(f'{CHOTUVE_MEDIA_URL}/video', json=body)
         self.assertEqual(response.status_code, 200)
         self.assertEqual({}, response.json)
 
