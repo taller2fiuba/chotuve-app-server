ca73420107d03c194998ab5dabfc215259993196
Lucho <sportelliluciano@gmail.com>
2020-06-10 01:21:10 -0300

    Agrego pruebas "unitarias" para comentarios

---
+++ src/tests/video/test_comentarios.py
@@ -0,0 +1,136 @@
+# pylint: skip-file
+import unittest
+import mock
+
+from tests.base import LoginMockTestCase
+from app import app, log
+
+CHOTUVE_MEDIA_URL = app.config.get('CHOTUVE_MEDIA_URL')
+
+class VideoComentariosTestCase(LoginMockTestCase):
+    def cargar_mock_obtener_video(self, mock, id):
+        mock.return_value.status_code = 200
+        mock.return_value.json = lambda: {
+            "descripcion": None,
+            "ubicacion": None,
+            "visibilidad": "publico",
+            "habilitado": True,
+            "_id": id,
+            "url": "chauchis",
+            "titulo": "holis",
+            "usuario_id": 1,
+            "duracion": 120,
+            "time_stamp": "2020-06-07T11:31:40.665Z",
+            "__v": 0
+        }
+        
+        return mock
+
+    @mock.patch('auth_server_api.obtener_usuarios')
+    @mock.patch('media_server_api.obtener_video')
+    def test_obtener_comentarios_video_sin_comentarios(self, mock_obtener_video, mock_auth):
+        self.cargar_mock_obtener_video(mock_obtener_video, "5edcd01cd3cf810031d865db")
+        mock_auth.return_value.status_code = 200
+        mock_auth.return_value.json = lambda: []
+
+        response = self.app.get("/video/5edcd01cd3cf810031d865db/comentario")
+        
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.json, [])
+    
+    @mock.patch('media_server_api.obtener_video')
+    def test_agregar_comentario_devuelve_201(self, mock):
+        self.cargar_mock_obtener_video(mock, "5edcd01cd3cf810031d865db")
+        body = {"comentario": "test"}
+
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            json=body)
+        self.assertEquals(response.status_code, 201)
+        self.assertEquals(response.json, {})
+    
+    @mock.patch('auth_server_api.obtener_usuarios')
+    @mock.patch('media_server_api.obtener_video')
+    def test_obtener_comentarios_video_con_comentarios(self, mock, mock_auth):
+        self.cargar_mock_obtener_video(mock, "5edcd01cd3cf810031d865db")
+        autor = {
+            "id": 1,
+            "nombre": "",
+            "apellido": "",
+            "email": "lucho@lucho.com",
+            "telefono": "",
+            "foto": ""
+        }
+        mock_auth.return_value.status_code = 200
+        mock_auth.return_value.json = lambda: [autor]
+
+        body = {"comentario": "test"}
+
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            json=body)
+        
+        self.assertEquals(response.status_code, 201)
+
+        response = self.app.get("/video/5edcd01cd3cf810031d865db/comentario")
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(response.json), 1)
+        self.assertEquals(response.json[0]["autor"], autor)
+        self.assertEquals(response.json[0]["comentario"], "test")
+        self.assertIn("fecha", response.json[0])
+        self.assertIsNotNone(response.json[0]["fecha"])
+
+    @mock.patch('media_server_api.obtener_video')
+    def test_agregar_comentario_con_content_type_erroneo_devuelve_400(self, mock):
+        self.cargar_mock_obtener_video(mock, "5edcd01cd3cf810031d865db")
+        
+        body = {"comentario": "test"}
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            data=body)
+        self.assertEquals(response.status_code, 400)
+    
+    @mock.patch('media_server_api.obtener_video')
+    def test_agregar_comentario_body_sin_clave_comentario_devuelve_400(self, mock):
+        self.cargar_mock_obtener_video(mock, "5edcd01cd3cf810031d865db")
+        body = {}
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            json=body)
+        self.assertEquals(response.status_code, 400)
+    
+    @mock.patch('media_server_api.obtener_video')
+    def test_agregar_comentario_vacio_devuelve_400(self, mock):
+        self.cargar_mock_obtener_video(mock, "5edcd01cd3cf810031d865db")
+        body = {"comentario": ""}
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            json=body)
+        self.assertEquals(response.status_code, 400)
+    
+    @mock.patch('media_server_api.obtener_video')
+    def test_agregar_comentario_nulo_devuelve_400(self, mock):
+        self.cargar_mock_obtener_video(mock, "5edcd01cd3cf810031d865db")
+        body = {"comentario": None}
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            json=body)
+        self.assertEquals(response.status_code, 400)
+    
+    @mock.patch('media_server_api.obtener_video')
+    def test_agregar_comentario_tipo_incorrecto_devuelve_400(self, mock):
+        self.cargar_mock_obtener_video(mock, "5edcd01cd3cf810031d865db")
+        body = {"comentario": 17}
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            json=body)
+        self.assertEquals(response.status_code, 400)
+        
+        body = {"comentario": {"a": "test"}}
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            json=body)
+        self.assertEquals(response.status_code, 400)
+    
+    @mock.patch('media_server_api.obtener_video')
+    def test_agregar_comentario_a_video_inexistente_devuelve_404(self, mock):
+        mock.return_value.status_code = 404
+        body = {"comentario": "test"}
+        response = self.app.post("/video/5edcd01cd3cf810031d865db/comentario", 
+            json=body)
+        self.assertEquals(response.status_code, 404)
+
+if __name__ == '__main__':
+    unittest.main()
