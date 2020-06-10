51d5557b4e822d701b1ed6109824369afa37237a
Lucho <sportelliluciano@gmail.com>
2020-06-10 01:20:47 -0300

    Borro un print, agrego un par de verificaciones sobre los datos recibidos

--- src/app/resources/video/video_comentario.py
+++ src/app/resources/video/video_comentario.py
@@ -1,5 +1,5 @@
 from flask import request, abort, g
-from app import db, log
+from app import db
 from app.login_requerido_decorator import login_requerido
 from app.models.comentario import Comentario, MAX_LEN_COMENTARIO
 import media_server_api
@@ -16,12 +16,12 @@ class VideoComentario(VideoBaseResource):
             abort(400)
 
         comentario = request.get_json().get('comentario')
-        if not comentario or len(comentario) >= MAX_LEN_COMENTARIO:
+        if not isinstance(comentario, str) or not 0 < len(comentario) <= MAX_LEN_COMENTARIO:
             return {"error": f'El comentario {comentario} es inválido'}, 400
 
         response = media_server_api.obtener_video(video_id)
         if response.status_code != 200:
-            abort(repsonse.status_code)
+            abort(response.status_code)
 
         db.session.add(Comentario(
             video=video_id,
@@ -43,8 +43,6 @@ class VideoComentario(VideoBaseResource):
             'ids': ','.join({str(comentario.usuario) for comentario in comentarios}),
             'cantidad': cantidad
         }
-
-        log.info(auth_server_api.obtener_usuarios(params).json())
         autores = {u['id']: u
                    for u in auth_server_api.obtener_usuarios(params).json()}
 
