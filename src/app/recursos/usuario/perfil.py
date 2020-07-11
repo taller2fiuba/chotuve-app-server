a89571551551fde03be128d9afbed00691cfa653
Matias <matiaseiglesias@yahoo.com>
2020-07-11 12:17:31 -0300

    agrego cantidad de videos a get perfil

--- src/app/recursos/usuario/perfil.py
+++ src/app/recursos/usuario/perfil.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask import request, g
 import auth_server_api
+import media_server_api
 from app.login_requerido_decorator import login_requerido
 from app.models.contacto import Contacto
 from app.models.solicitud_contacto import SolicitudContacto
@@ -17,6 +18,8 @@ class PerfilUsuarioResource(Resource):
 
         perfil = response.json()
         perfil['cantidad-contactos'] = Contacto.obtener_cantidad_contactos(usuario_id)
+        videos = media_server_api.obtener_videos_usuario(usuario_id, 0, 0).json()
+        perfil["cantidad-videos"] = videos["cantidad_de_videos"]
 
         if usuario_id != g.usuario_actual:
             if Contacto.es_contacto(g.usuario_actual, usuario_id):
